567ce059e62def71f420588e00bb5603fb7c8643
alexforsale <alexforsale@yahoo.com>
2014-05-30 03:41:59 +0700

    caf setup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include device/qcom/msm7627a/BoardConfig.mk
+
+USE_CAMERA_STUB := true
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := eg909
+
+BOARD_HAS_ATH_WLAN := true
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_2_0_DEV
+HOSTAPD_VERSION := VER_2_0_DEV
+WIFI_CFG80211_DRIVER_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
+WIFI_CFG80211_DRIVER_MODULE_NAME := "cfg80211"
+WIFI_CFG80211_DRIVER_MODULE_ARG  := ""
+WIFI_TEST_INTERFACE     := "sta"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50001
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+
+BOARD_EGL_CFG := device/qcom/msm7627a/egl.cfg
+
+BOARD_USES_QCOM_HARDWARE := true
+DYNAMIC_SHARED_LIBV8SO := true
+USE_OPENGL_RENDERER := true
+TARGET_USES_MDP3 := true
+BOARD_HAVE_QCOM_FM := true
+PROTEUS_DEVICE_API := true
+
+BOARD_KERNEL_BASE    := 0x00e3b004
+
+TARGET_PREBUILT_KERNEL := device/hisense/eg909/kernel
+
+# webkit
+ENABLE_WEBGL := true
+WEBCORE_FPS_DISPLAY := true
+ENABLE_WEBAUDIO := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+ENABLE_SVG := true
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/hisense/eg909
+
+-include vendor/hisense/eg909/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -13,3 +13,86 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/hisense/eg909/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+DEVICE_PACKAGE_OVERLAYS := device/hisense/eg909/overlay
+
+# dsds enable
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dsds.enabled=true
+
+# property override for dns
+PRODUCT_PROPERTY_OVERRIDES += \
+    dhcp.wlan0.dns1=8.8.8.8 \
+    dhcp.wlan0.dns2=8.8.4.4 \
+    net.dns1=8.8.8.8 \
+    net.dns2=8.8.4.4 \
+    net.wlan0.dns1=8.8.8.8 \
+    net.wlan0.dns2=8.8.4.4
+
+# property override for browsermanagement
+PRODUCT_PROPERTY_OVERRIDES += \
+    browser.management=false
+
+# property override for google client id
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-hisense
+
+# property override for checkjni in every type of build
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0
+
+# property override for radio.adb
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.radio.adb_log_on=1
+
+# for default.prop override in every type of build
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.secure=0 \
+    ro.debuggable=1 \
+    ro.allow.mock.location=0
+
+# for DSP presets
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=false
+
+# setupwizard
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.mode=OPTIONAL
+
+# init files
+PRODUCT_PACKAGES += \
+    init.rc \
+    init.eg909.rc
+
+# packages for init.rc
+PRODUCT_PACKAGES += \
+    resize2fs \
+    e2fsck
+
+# permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
+
+# proprietary stuffs
+$(call inherit-product-if-exists, vendor/hisense/eg909/eg909-vendor-blobs.mk)
+
+# gps.conf
+$(call inherit-product, device/common/gps/gps_as_supl.mk)
+
+# gapps
+#$(call inherit-product-if-exists, vendor/gapps/gapps-vendor-blobs.mk)
+
+PRODUCT_DEVICE := eg909
+PRODUCT_NAME := my4ndr0id_eg909
+PRODUCT_BRAND := hisense
+PRODUCT_MODEL := Hacksense eg909
+PRODUCT_MANUFACTURER := Hisense
